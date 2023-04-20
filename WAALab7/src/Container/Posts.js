16cb0d31ed4ac33f3a567a501651a2afd8e14452
mgniguse <91496765+mgniguse@users.noreply.github.com>
2023-04-20 16:53:40 -0500

    lab 8

--- WAALab7/src/Container/Posts.js
+++ WAALab7/src/Container/Posts.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import Post from '../Component/Post';
+import React from "react";
+import Post from "../Component/Post";
 
 function Posts(props) {
-  
-  return(
+  return (
     <div>
-      {props.data.map(p=>{
-        return <Post id={p.id} key={p.id}title={p.title} author={p.author}/>
-      })
-      }
+      {props.data.map((p) => {
+        return <Post 
+        id={p.id} 
+        key={p.id} 
+        title={p.title} 
+        author={p.author} 
+        />;
+      })}
     </div>
-   
-  )
+  );
 }
 
-export default Posts
+export default Posts;
